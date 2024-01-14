7f1848dd00cb851b9c6f37cee2c321420209ace5
ErikOwsiak <erik@iotech.systems>
2024-01-14 19:01:23 +0100

    init

---
+++ core/radioLink.py
@@ -0,0 +1,15 @@
+
+# import LoRaRF as lora
+from LoRaRF import SX127x
+
+
+class radioLink(object):
+
+   RST_PIN: int = 22
+
+   def __init__(self):
+      self.lnk: SX127x = SX127x()
+      self.lnk.setPins(reset=radioLink.RST_PIN)
+
+   def init(self):
+      self.lnk.begin()
