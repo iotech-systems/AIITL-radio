e0437353c6effa8144e53d9c885606855bbab260
ErikOwsiak <erik@iotech.systems>
2024-01-16 11:48:19 +0100

    init

--- radio/sx127x/loraSPI.py
+++ radio/sx127x/loraSPI.py
@@ -1,60 +1,46 @@
 
 import spidev, typing as t
-import time, RPi.GPIO as GPIO
 
-GPIO.setmode(GPIO.BCM)
+
 # -- dev / 6 --
 SPI_SPEED: int = 3900000
 
 
 class loraSPI(object):
 
-   def __init__(self, pin_rst: int = 22, pin_cs: int = 8
-         , bus: int = 0, bus_dev: int = 0, speed: int = SPI_SPEED):
-      # -- -- -- --
+   def __init__(self, bus: int = 0, bus_dev: int = 0, bus_hz: int = SPI_SPEED):
       try:
-         self.pin_rst: int = pin_rst
-         self.pin_cs: int = pin_cs
          self.bus = bus
          self.dev = bus_dev
-         self.speed = speed
+         self.spd_hz = bus_hz
          self.mode: int = 0
          self.lsbfst: bool = False
-         # -- init pins --
-         GPIO.setup([self.pin_rst, self.pin_cs], GPIO.OUT)
-         GPIO.output([self.pin_rst, self.pin_cs], GPIO.LOW)
+         self.spi: spidev.SpiDev = spidev.SpiDev()
       except Exception as e:
          print(e)
       finally:
          pass
 
    def dump(self):
-      spi: spidev.SpiDev = spidev.SpiDev()
-      spi.open(bus=self.bus, device=self.dev)
-      spi.max_speed_hz = self.speed
-      print(f"lsbfst: {spi.lsbfirst} | mode: {spi.mode} | hz: {spi.max_speed_hz}")
-      spi.close()
-
-   def rst(self):
-      pass
+      self.spi.open(bus=self.bus, device=self.dev)
+      self.spi.max_speed_hz = self.spd_hz
+      print(f"lsbfst: {self.spi.lsbfirst} | mode: {self.spi.mode} | hz: {self.spi.max_speed_hz}")
+      self.spi.close()
 
    def init(self):
-      spi: spidev.SpiDev = spidev.SpiDev()
-      spi.open(bus=self.bus, device=self.dev)
-      spi.max_speed_hz = self.speed
-      print(f"lsbfst: {spi.lsbfirst} | mode: {spi.mode} | hz: {spi.max_speed_hz}")
-      spi.close()
+      self.spi.open(bus=self.bus, device=self.dev)
+      self.spi.max_speed_hz = self.spd_hz
+      print(f"lsbfst: {self.spi.lsbfirst} | mode: {self.spi.mode} | hz: {self.spi.max_speed_hz}")
+      self.spi.close()
 
-   def transfer(self, buff: t.Iterable) -> tuple:
-      spi = spidev.SpiDev()
+   def xtfr2(self, buff: t.Iterable) -> tuple:
       try:
-         spi.open(self.bus, self.dev)
-         spi.max_speed_hz = self.speed
-         spi.lsbfirst = self.lsbfst
-         spi.mode = self.mode
-         ret = spi.xfer2(buff)
-         return ret
+         self.spi.open(self.bus, self.dev)
+         self.spi.max_speed_hz = self.spd_hz
+         self.spi.lsbfirst = self.lsbfst
+         self.spi.mode = self.mode
+         return self.spi.xfer2(buff)
       except Exception as e:
          print(e)
       finally:
-         spi.close()
+         self.spi.close()
--- radio/sx127x/sx127x.py
+++ radio/sx127x/sx127x.py
@@ -11,6 +11,10 @@ from sx127xRegs import sx127xRegs as regs
 from sx127xConsts import sx127xConsts as consts
 
 
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+
+
 class sx127x(object):
 
    def __init__(self, spi: loraSPI, rst_pin: int, cs_pin: int):
@@ -18,25 +22,24 @@ class sx127x(object):
       self.rst_pin: int = rst_pin
       self.cs_pin: int = cs_pin
       self._modem: int = 0
+      GPIO.setup([self.rst_pin, self.cs_pin], GPIO.OUT)
 
    def init(self):
-      GPIO.setwarnings(False)
-      GPIO.setmode(GPIO.BCM)
-      GPIO.setup(self.rst_pin, GPIO.OUT)
-      GPIO.setup(self.cs_pin, GPIO.OUT)
       self.reset()
-      self.setModem(consts.LORA_MODEM)
+      # self.setModem(consts.LORA_MODEM)
 
    def reset(self):
       GPIO.output(self.rst_pin, GPIO.LOW)
       time.sleep(0.001)
       GPIO.output(self.rst_pin, GPIO.HIGH)
       time.sleep(0.005)
-      t = time.time()
+
+   def chip_ver(self):
       ver = 0x00
+      t = time.time()
       while ver != 0x12 and ver != 0x22:
          ver = self.readRegister(regs.REG_VERSION)
-         if time.time() - t > 8:
+         if time.time() - t > 4:
             return False
          print(f"[ ver: {ver}]")
       return True
@@ -55,10 +58,10 @@ class sx127x(object):
       return self._transfer(address & 0x7F, 0x00)
 
    def _transfer(self, address: int, data: int) -> int:
-      buff = [address, data]
-      print(f"[ spi sending: {buff} ]")
+      buff_arr = [address, data]
+      print(f"[ spi sending: {buff_arr} ]")
       self.__set_cs(GPIO.LOW)
-      rval = self.spi.transfer(buff)
+      rval = self.spi.transfer(buff_arr)
       self.__set_cs(GPIO.HIGH)
       print(f"[ rval: {rval}]")
       if len(rval) == 2:
