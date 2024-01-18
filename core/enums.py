13228932d8e061ac8cd4b408d04969db61539c4a
ErikOwsiak <erik@iotech.systems>
2024-01-18 13:21:56 +0100

    adding sxcli

---
+++ core/enums.py
@@ -0,0 +1,21 @@
+
+import enum
+
+class TCOLORS(enum.IntEnum):
+   black = 30
+   grey = 30         # Actually black but kept for backwards compatibility
+   red = 31
+   green = 32
+   yellow = 33
+   blue = 34
+   magenta = 35
+   cyan = 36
+   light_grey = 37
+   dark_grey = 90
+   light_red = 91
+   light_green = 92
+   light_yellow = 93
+   light_blue = 94
+   light_magenta = 95
+   light_cyan = 96
+   white = 97
